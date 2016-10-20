fa192c5328cae319599d7a3a4105b15b321c1d33
aklump <sourcecode@intheloftstudios.com>
2016-10-20 12:32:21 -0700

    slight formatting change.

--- includes/RenderPatternsPattern.php
+++ includes/RenderPatternsPattern.php
@@ -6,34 +6,38 @@
 
 /**
  * Represents a Pattern object class.
- * 
+ *
  * @brief An abstract base for all Pattern classes.
  */
-abstract class RenderPatternsPattern implements RenderPatternsPatternInterface {
+abstract class RenderPatternsPattern implements RenderPatternsPatternInterface
+{
+    protected $vars;
 
-  protected $vars;
+    public function __get($key)
+    {
+        $d = $this->defaults();
+        $default = isset($d[$key]) ? $d[$key] : null;
 
-  public function __set($key, $value) {
-    $this->vars[$key] = $value;
-  }
-
-  public function __get($key) {
-    $d = $this->defaults();
-    $default = isset($d[$key]) ? $d[$key] : NULL;
-    
-    return isset($this->vars[$key]) ? $this->vars[$key] : $default;
-  }
-
-  public function __isset($key) {
-    return array_key_exists($key, $this->defaults());
-  }
-
-  public function render() {
-    if (!function_exists('drupal_render')) {
-      throw new \RuntimeException("Missing function drupal_render().");
+        return isset($this->vars[$key]) ? $this->vars[$key] : $default;
     }
-    $build = $this->build();
-    
-    return drupal_render($build);
-  }
+
+    public function __set($key, $value)
+    {
+        $this->vars[$key] = $value;
+    }
+
+    public function __isset($key)
+    {
+        return array_key_exists($key, $this->defaults());
+    }
+
+    public function render()
+    {
+        if (!function_exists('drupal_render')) {
+            throw new \RuntimeException("Missing function drupal_render().");
+        }
+        $build = $this->build();
+
+        return drupal_render($build);
+    }
 }
